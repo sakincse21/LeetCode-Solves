4b56076789c473f2e3573f9f227d944a109575ab
Saleheen Uddin Sakin <sakin.cse21@gmail.com>
2025-02-21 12:34:34 +0600

    https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150

---
+++ majority-element(moore voting).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        //moore voting technique
+        int count=0; int result=0;
+        for(auto num:nums){
+            if(count==0) result=num;
+            if(num==result) count++;
+            else count--;
+        }
+        return result;
+    }
+};
